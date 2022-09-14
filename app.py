3b9bdc8828c8d9605dfe1e9915f26a01a204fdf8
Ajay Kumar <ajay_kumar_ggn@mckinsey.com>
2022-09-14 10:15:08 +0530

    changed app

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
- 
-app = Flask(__name__)
+from flask import Flask,render_template,redirect
+from forms import LoginForm
 
-@app.route('/')
-def hello_world():
-    return '<h2>This is my first app by Ajay Kumar</h2>'
+app = Flask(__name__)
+app.config.update(
+    TESTING=True,
+    SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+)
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    return "<h1>running workflow</h1>"
+
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
